dbfd131cccc5856738c1d7c9f3f48914e636ba2f
alfredo robles <alfredoroblesing@gmail.com>
2019-11-02 15:52:45 -0600

    rutas etc

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+//npm init
+//npm i express
+// sudo npm install -g nodemon
+//npm i body-parser
 const bodyParser = require('body-parser');
+
+const response = require('./network/response');
+
 const router = express.Router();
 
 var app = express();
@@ -12,17 +19,37 @@ app.use(router);
 
 router.get('/ruta1', function (req, res) {
     console.log(req.query);
-    res.send('get response');
+    console.log(req.headers);
+    res.header({
+        "Autorisation": "Bearer aunt....",
+    });
+    // res.send('get response');
+    response.success(req, res, 'primera respuesta', 200);
+
 });
 
 router.post('/ruta2', function (req, res) {
     console.log(req.body);
-    res.send('post response');
+    if (req.query.error === "ok") {
+        response.error(req, res, "error 1", 500, 'algo salio mal');
+    } else {
+        response.success(req, res, 'create post', 201)
+    }
 });
 
 router.put('/', function (req, res) {
-    res.send('put response');
+    response.success(req, res, 'create');
 });
+
+router.delete('/delete', function (req, res) {
+    console.log(req.query);
+    console.log(req.body);
+    //  res.status(201).send({error: '', body: 'eliminado correctamente'});
+    response.success(req, res, 'delete');
+
+});
+
+app.use('/app', express.static('public'));
 
 
 app.listen(3000);
