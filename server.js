9a75957ca6fc39264be33491c4c43aa4d2dea5e4
alfredo robles <alfredoroblesing@gmail.com>
2019-11-01 18:48:20 -0600

    cambio a master

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
 var app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
 
-app.use('/', function (req, res) {
+app.use(router);
 
-    res.send('hello word');
-
-
+router.get('/ruta1', function (req, res) {
+    console.log(req.query);
+    res.send('get response');
 });
 
+router.post('/ruta2', function (req, res) {
+    console.log(req.body);
+    res.send('post response');
+});
+
+router.put('/', function (req, res) {
+    res.send('put response');
+});
+
+
 app.listen(3000);
-console.log('aplicacion enable');
+console.log('aplicacion enable => http://localhost:3000');
+
+
+
